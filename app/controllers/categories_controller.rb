0411b0e5d284c4f8c1fe08959f56fcc1a98b0921
Ikaro Souza <ikarolp@hotmail.com>
2017-05-28 23:13:16 -0300

    Integração do front-end com o back-end

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,6 +10,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
+    @services = Service.where(category: Category.find(params[:id])).all
   end
 
   # GET /categories/new
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :services
   resources :categories
   devise_for :users
-  root to: redirect('/categories')
+  root to: redirect('/services')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
